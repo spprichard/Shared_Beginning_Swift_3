f54ac4bc09d690eda24aa866d619ce3f4fdd5d9f
Steven Prichard <sprichard@paulbunyan.net>
2016-11-18 11:06:44 -0600

    init commit

---
+++ Begining Swift.playground/Contents.swift
@@ -0,0 +1,70 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//
+//var fantasyPlayer = (name: "TJ Yelden", fantasyPoints: 123, rank: 100, onMyTeam: false)
+//
+//
+//fantasyPlayer.0
+//fantasyPlayer.1
+//fantasyPlayer.2
+//fantasyPlayer.3
+//
+//fantasyPlayer.name
+//fantasyPlayer.fantasyPoints
+//fantasyPlayer.rank
+//fantasyPlayer.onMyTeam
+//
+//
+//if fantasyPlayer.onMyTeam {
+//    print("\(fantasyPlayer.name) is on your team")
+//}else{
+//    print("\(fantasyPlayer.name) is NOT on your team")
+//}
+
+/////////////////////////
+
+////// Optionals  ///////
+
+/////////////////////////
+
+
+// If Let //
+//var authorName : String? = "Stephen King"
+//
+//if let unwrappedAuthor = authorName{
+//    print("The Author's name is \(unwrappedAuthor)")
+//}else{
+//    print("No Author...")
+//}
+
+
+
+//func sayName(firstName: String? , lastName : String?){
+//    guard let firstName = firstName, let lastName = lastName else {
+//        print("exiting...")
+//        return
+//    }
+//    print("Hello \(firstName) \(lastName)")
+//}
+//
+//sayName(firstName: "Steven", lastName: "Prichard")
+
+
+
+/////////////////////////
+
+////// Arrays  ///////
+
+/////////////////////////
+
+
+
+
+
+
+
+
+
---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Steven Prichard on 11/17/16.
+//  Copyright © 2016 Steven Prichard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func buttonPress(_ sender: Any) {
+        print("Hello World")
+    }
+}
+
