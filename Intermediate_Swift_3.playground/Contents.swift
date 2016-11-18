599be6b11ed5e91aeefd1288962673ec176c505b
Steven Prichard <sprichard@paulbunyan.net>
2016-11-18 15:20:00 -0600

    added closures section

--- Intermediate_Swift_3.playground/Contents.swift
+++ Intermediate_Swift_3.playground/Contents.swift
@@ -9,4 +9,21 @@ import UIKit
 ////// Closures /////
 /////////////////////
 
+var multClosure = {
+    (number : Int , multiplier : Int) -> Int
+    in
+    return number * multiplier
+}
+
+multClosure(2,6)
+
+
+//compact closer
+
+var addClosure : (Int, Int) -> Int
+addClosure = {
+    $0 + $1
+}
+
+addClosure(2,2)
 
