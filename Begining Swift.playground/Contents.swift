bac0a5c651a9e17d5e96dc58cb4d194d267ef976
Steven Prichard <sprichard@paulbunyan.net>
2016-11-18 12:19:33 -0600

    Finished video 12

--- Begining Swift.playground/Contents.swift
+++ Begining Swift.playground/Contents.swift
@@ -56,13 +56,70 @@ import UIKit
 
 /////////////////////////
 
-////// Arrays  ///////
+////// Dictionaries  /////
 
 /////////////////////////
 
+//WebDev Dictionary
+//Key: Name
+//Value: Boolean, if that person is here they get a true, if they are not they get a false
+
+//var webDev = ["Nate": true, "Nelson" : false, "Steven" : true, "Simon" : true ]
+//
+//webDev["Nate"]
+//webDev["Simon"] = nil
+//
+//for (key,value) in webDev{
+//    print("Is \(key) here? \(value) ")
+//}
+//
+//for key in webDev.keys{
+//    print("\(key) is a part of the Web Development Team!")
+//}
+//
+//
+//print("Foo: \(webDev["Nate"])")
 
 
 
+///*:
+// #### Beginnng Swift Video Tutorial Series - raywenderlich.com
+// #### Video 12: Dictionaries
+// 
+// **Note:** If you're seeing this text as comments rather than nicely-rendered text, select Editor\Show Rendered Markup in the Xcode menu.
+// */
+//
+////: Create a new dictionary and add the following values: Stephen King - Under the Dome, Elizabeth Peters - Crocodile on the Sandbank, Clive Cussler - The Wrecker
+//var books = ["Stephen King": "Under the Dome", "Elizabeth Peters" : "Crocodile on the Sandbank", "Clive Cussler" : "THe Wrecker"]
+////: Add a new key: Robert Heinlein - The Moon is a Harsh Mistress
+//books["Robert Heinlein"] =  "The Moon is a Harsh Mistress"
+////: Print out the Stephen King value
+//if let kingsBook = books["Stephen King"]{
+//    print(kingsBook)
+//}
+////: Now delete the Stephen King key/value pair
+//books["Stephen King"] = nil
+//
+////: Loop through the dictionary and print out all the key/values
+//for (key,value) in books{
+//    print("\(key) wrote \(value)")
+//}
+////: **Ub3r H4ck3r Challenge** Declare a function occurrencesOfCharacters that calculates which characters occur in a string, as well as how often each of these characters occur. Return the result as a dictionary: func occurrencesOfCharacters(text: String) -> [Character: Int]
+//func occurancesOfCharacters(text: String) -> [Character : Int]{
+//    var occuranceDictionary: [Character : Int] = [:]
+//    
+//    for letter in text.characters{
+//        if occuranceDictionary[letter] != nil {
+//            occuranceDictionary[letter] = occuranceDictionary[letter]! + 1
+//        }
+//        else{
+//            occuranceDictionary[letter] = 1
+//        }
+//    }
+//    return occuranceDictionary
+//}
+//
+//print(occurancesOfCharacters(text: "fdsvgkhjlker;fhvcbvjegfldbcjnwdsihuuwdcjneuvyb owdjbcerwufjced"))
 
 
 
