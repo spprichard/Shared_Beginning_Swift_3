3ff296dfe098ffb4b057dc620867be115b82f9bf
Steven Prichard <sprichard@paulbunyan.net>
2016-11-22 13:15:18 -0600

    finished init challenge

--- Intermediate_Swift_3.playground/Contents.swift
+++ Intermediate_Swift_3.playground/Contents.swift
@@ -404,12 +404,35 @@ import UIKit
  */
 
 //: Create a story class that contains two string properties: name and a writer. Provide an a init() that sets both properties
+class Story {
+    var name : String
+    var writer : String
+    
+    init(name : String, writer : String) {
+        self.name = name
+        self.writer = writer
+    }
+}
+
 
 
 //: Create a new Movie class that is a sublcass of the story class. Give it a new string propery called directory. Create a init that sets the director, name, and writer properties.
 
+class Movie : Story {
+    var director : String
     
-    //: Create a convenience init that takes just a director's name. Set the name to "Unknown Title" and the writer to "Unknown writer"
+    init(director : String, name : String, writer : String) {
+        self.director = director
+        super.init(name: name, writer: writer)
+    }
+    
+    convenience init(director : String){
+        self.init(director: director, name: "Unknown Title", writer: "Unknown Writer")
+    }
+}
+
+
+//: Create a convenience init that takes just a director's name. Set the name to "Unknown Title" and the writer to "Unknown writer"
     
 
 
